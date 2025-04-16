9273b31284625154f92ea0fbd3e1b941dd234c45
sajal amin <sajalamin67@gmail.com>
2025-04-16 09:06:29 -0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,49 @@
+print("hello")
+
+
+print("Hello Sajal!")
+
+# This command takes input from the user and gives output .
+name = input("Whats your name?") 
+print("Hello," , name )
+
+# Initilizing array in python
+
+
+
+fruits = ["apple " , "bnana" , "orange"]
+
+
+print("fruits")
+
+# adding two numbers
+a = 10
+b = 20
+c = a + b
+print("The sum of two numbers is : " ,  c)
+
+print("Hello, sajal")
+
+
+
+# Initilizing array in py
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+
+# adding two numbers
+a = 10
+b = 20
+c = a + b
+print("The sum of two numbers is: ", c)
+
+
+# List
+a = ["abc", "def", "ghi", "jkl"]
+a[1] = "xyz"
+print(a)
+# output: ['abc', 'xyz', 'ghi', 'jkl']
+
+# Tuple
+b = (1, 3, 4, 6)
+b[1] = 99
+print(b)
